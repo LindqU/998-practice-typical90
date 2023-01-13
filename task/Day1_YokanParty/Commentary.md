8e6d3c0cf065bb9623fcbd51b0796ef28c5430fa
linda <yuu.lindq@gmail.com>
2023-01-14 00:48:47 +0900

    del pycashe

---
+++ task/Day1_YokanParty/Commentary.md
@@ -0,0 +1,3 @@
+# 問題
+[001 - Yokan Party（★4）](https://atcoder.jp/contests/typical90/tasks/typical90_a)
+
--- task/Day1_YokanParty/yokan_party.py
+++ task/Day1_YokanParty/yokan_party.py
@@ -3,40 +3,12 @@ from sys import stdin
 
 
 def main():
-    N, L= map(int, stdin.readline().split())
-    K = int(stdin.readline())
-    A = list(map(int, stdin.readline().split()))
-
-    A.insert(0,0)
-    A.append(L)
-    maxLen = L//(K+1)
-    minLen = 0
-
-    def check(halfLen):
-        cnt = 0
-        work_a = 0
-        for a in A:
-            if a - work_a < halfLen:
-                continue
-            else:
-                work_a = a
-                cnt += 1
-        if cnt >= K+1:
-            return True
-        else:
-            return False
-
-    while minLen != maxLen:
-        halfLen = ((minLen+maxLen)//2) + 1
-        if check(halfLen):
-            minLen = halfLen
-        else:
-            maxLen = halfLen -1 
-
-    print(minLen)
+    N, L = map(int, input().split())
+    K = int(input())
+    
 
 
-
+    # 先頭に0、末尾にLを追加することで切れ目の差を求めやすくする
 
 if __name__ == "__main__":
     main()
